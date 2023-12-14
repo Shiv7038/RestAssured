054d415993e5f8ef14dadcf213d5a332c9969388
spawar6 <shivaji.pawar@morningstar.com>
2023-12-14 20:16:50 +0530

    first

---
+++ src/test/java/Basic/BasicMetohds.java
@@ -0,0 +1,38 @@
+package Basic;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class BasicMetohds {
+
+	
+	@Test
+	public void getAPIMethod() {
+		
+		RestAssured.baseURI="https://reqres.in/";
+		
+		given().relaxedHTTPSValidation().when().get("api/users?page=2").then().log().all();
+		
+		Response resonse=given().log().all().when().get("users?page=2").then().extract().response(); 
+        // to get the response in Response format
+		
+		JsonPath js=resonse.jsonPath();
+		String a=js.getString("data.id");
+		System.out.println(a);
+		
+		String stringResonse=given().log().all().when().get("users?page=2").then().extract()
+                .response().asPrettyString();
+           // to get response in String format
+
+       String stringResonse1=given().queryParam("page",2).when().get("api/users").then().extract()
+                 .response().asPrettyString();
+            // to get response in PrittyString format
+        System.out.println(stringResonse1);
+
+	}
+}
