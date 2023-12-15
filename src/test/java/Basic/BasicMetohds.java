ebdc9632d48ba6f92fccd65167c0fcd615c5a366
Shivaji Pawar <shivaji.pawar@morningstar.com>
2023-12-15 20:52:15 +0530

    methods

--- src/test/java/Basic/BasicMetohds.java
+++ src/test/java/Basic/BasicMetohds.java
@@ -11,20 +11,20 @@ import static io.restassured.RestAssured.*;
 public class BasicMetohds {
 
 	
-	@Test
+	@Test(enabled = false)
 	public void getAPIMethod() {
-		
+
 		RestAssured.baseURI="https://reqres.in/";
-		
+
 		given().relaxedHTTPSValidation().when().get("api/users?page=2").then().log().all();
-		
-		Response resonse=given().log().all().when().get("users?page=2").then().extract().response(); 
+
+		Response resonse=given().log().all().when().get("users?page=2").then().extract().response();
         // to get the response in Response format
-		
+
 //		JsonPath js=resonse.jsonPath();
 //		String a=js.getString("data.id");
 //		System.out.println(a);
-//		
+//
 		String stringResonse=given().log().all().when().get("users?page=2").then().extract()
                 .response().asPrettyString();
            // to get response in String format
@@ -32,10 +32,40 @@ public class BasicMetohds {
        String stringResonse1=given().queryParam("page",2).when().get("api/users").then().extract()
                  .response().asPrettyString();
             // to get response in PrittyString format
-       
-       
         System.out.println(stringResonse1);
 
+	}
 
+	@Test
+	public void postAPIMethod() {
+
+		RestAssured.baseURI="https://reqres.in/";
+
+		given().body("{\n" +
+				"    \"name\": \"morpheus\",\n" +
+				"    \"job\": \"leader\"\n" +
+				"}").when().post("api/users").then().log().all();
+	}
+
+	@Test
+	public void putAPIMethod() {
+
+		RestAssured.baseURI="https://reqres.in/";
+
+		given().body("{\n" +
+				"    \"name\": \"morpheus\",\n" +
+				"    \"job\": \"zion resident\"\n" +
+				"}").when().put("api/users/2").then().log().all();
+	}
+
+	@Test
+	public void deleteAPIMethod() {
+
+		RestAssured.baseURI="https://reqres.in/";
+
+		given().body("{\n" +
+				"    \"name\": \"morpheus\",\n" +
+				"    \"job\": \"zion resident\"\n" +
+				"}").when().delete("api/users/2").then().log().all();
 	}
 }
